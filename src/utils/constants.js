f9bc665c07fd81f4ba918573b22f90bc4cf6cc1f
tye-hoff <tyler.hoffmann5654@gmail.com>
2025-07-29 12:35:09 -0400

    Revert "Stage 1 frontend and api"

--- src/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const APIkey = "MaFoIgSXeUpXNsLkJWOo6EsvpxSN5owEB1D0VEPB";
-
-const clientId = "SOmNWsRRmNrl67WsoLRt";
-
-// const authCode = "ybXU2VeUmUniOOBQGACluwRG5EWYRe";
-
-const baseURL = "https://freesound.org/apiv2";
-
-export { APIkey, baseURL, clientId };
